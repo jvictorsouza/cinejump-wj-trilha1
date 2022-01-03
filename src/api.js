bbd69a06c75118b4209db0cbd90f5fb68e4c4dd8
joao.chagas <joao.chagas@webjump.com.br>
2022-01-03 09:46:08 -0300

    insert api functions and render functions of new modules

--- src/api.js
+++ src/api.js
@@ -1,7 +1,53 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.github.com",
+  baseURL: `${process.env.API_BASE_URL}`,
+  headers: {
+    api_key: `${process.env.API_KEY}`,
+    language: "pt-BR",
+    region: "BR",
+  },
 });
 
-export default api;
+const getPopularMovies = async () => {
+  const returnApi = await api
+    .get(`/movie/popular`, { headers: { page: "1" } })
+    .then((response) => response)
+    .then((data) => data.results)
+    .catch((error) => console.log(error));
+  return returnApi;
+};
+
+const getPlayingMovies = async () => {
+  const returnApi = await api
+    .get(`/movie/now_playing`, { headers: { page: "1" } })
+    .then((response) => response)
+    .then((data) => data.results)
+    .catch((error) => console.log(error));
+  return returnApi;
+};
+
+const getRecomendationsMovies = async (movie_id) => {
+  const returnApi = await api
+    .get(`/movie/${movie_id}/recommendations`, { headers: { page: "1" } })
+    .then((response) => response)
+    .then((data) => data.results)
+    .catch((error) => console.log(error));
+  return returnApi;
+};
+
+const getTopMovies = async () => {
+  const returnApi = await api
+    .get(`/movie/top_rated`, { headers: { page: "1" } })
+    .then((response) => response)
+    .then((data) => data.results)
+    .catch((error) => console.log(error));
+  return returnApi;
+};
+
+export {
+  getPopularMovies,
+  getPlayingMovies,
+  getRecomendationsMovies,
+  getTopMovies,
+};
